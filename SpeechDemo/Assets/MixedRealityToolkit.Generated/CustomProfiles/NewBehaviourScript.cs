2d6b2eee55d13dc29f34599de5eb583e0ef6e28a
rlbogue <rob.bogue@thorprojects.com>
2020-08-02 10:48:39 -0400

    Initial Checkin
    Two Samples

---
+++ SpeechDemo/Assets/MixedRealityToolkit.Generated/CustomProfiles/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour, IMixedRealitySpeechHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        CoreServices.InputSystem?.RegisterHandler<IMixedRealitySpeechHandler>(this);
+    }
+
+    public void OnSpeechKeywordRecognized(SpeechEventData eventData)
+    {
+        switch (eventData.Command.Keyword.ToLower())
+        {
+            case "hello":
+                Debug.Log("Hello World!");
+                break;
+            default:
+                Debug.Log($"Unknown option {eventData.Command.Keyword}");
+                break;
+        }
+    }
+}
---
+++ SpinningCube/Assets/Rotator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public Vector3 RotationXYZ = new Vector3(1.0f, 2.0f, 3.0f);
+    public float RotationRateScale = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float totalScale = Time.deltaTime * RotationRateScale;
+        Vector3 v3 = new Vector3(RotationXYZ.x * totalScale, RotationXYZ.y * totalScale, RotationXYZ.z * totalScale);
+        gameObject.transform.Rotate(v3);
+    }
+}
